f5f7f91ae5feae0fa6d50c0bb16e7e5e74d73593
Mohamed Salama <117160810+MohamedSalama25@users.noreply.github.com>
2025-12-14 12:53:21 +0200

    Delete TravelcoApi directory

--- TravelcoApi/src/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express=require('express');
-const router=express.Router();
-const AuthControllers=require('../controllers/authControllers');
-
-router.post('/register',AuthControllers.register)
-router.post('/login',AuthControllers.login)
-
-module.exports=router;
